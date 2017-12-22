60b6f5d70c6ef674c7ec09cfd0acc8f42274b6f2
Ferdinand Hill <ferdi@smsportal.co.za>
2017-12-22 14:27:25 +0200

    added vue stuff

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="hello">
+    <div class="mdc-toolbar mdc-toolbar--fixed">
+      <div class="mdc-toolbar__row">
+        <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+          <span>
+            <a href="/" class="mdc-toolbar__menu-icon"><i class="material-icons">&#xE5C4;</i></a>
+          </span>
+          <button class="demo-menu material-icons mdc-toolbar__menu-icon">menu</button>
+          <span class="mdc-toolbar__title catalog-title">Temporary Drawer</span>
+        </section>
+      </div>
+    </div>
+
+    <aside class="mdc-temporary-drawer">
+      <nav class="mdc-temporary-drawer__drawer">
+        <header class="mdc-temporary-drawer__header">
+          <div class="mdc-temporary-drawer__header-content mdc-theme--primary-bg mdc-theme--text-primary-on-primary">
+            Header here
+          </div>
+        </header>
+        <nav class="mdc-temporary-drawer__content mdc-list-group">
+          <div id="icon-with-text-demo" class="mdc-list">
+            <a class="mdc-list-item mdc-list-item--activated" href="#">
+              <i class="material-icons mdc-list-item__graphic" aria-hidden="true">inbox</i>Inbox
+            </a>
+            <a class="mdc-list-item" href="#">
+              <i class="material-icons mdc-list-item__graphic" aria-hidden="true">star</i>Star
+            </a>
+            <a class="mdc-list-item" href="#">
+              <i class="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>Sent Mail
+            </a>
+            <a class="mdc-list-item" href="#">
+              <i class="material-icons mdc-list-item__graphic" aria-hidden="true">drafts</i>Drafts
+            </a>
+          </div>
+
+          <hr class="mdc-list-divider">
+
+          <div class="mdc-list">
+            <a class="mdc-list-item" href="#">
+              <i class="material-icons mdc-list-item__graphic" aria-hidden="true">email</i>All Mail
+            </a>
+            <a class="mdc-list-item" href="#">
+              <i class="material-icons mdc-list-item__graphic" aria-hidden="true">delete</i>Trash
+            </a>
+            <a class="mdc-list-item" href="#">
+              <i class="material-icons mdc-list-item__graphic" aria-hidden="true">report</i>Spam
+            </a>
+          </div>
+        </nav>
+      </nav>
+    </aside>
+    <main class="demo-main mdc-toolbar-fixed-adjust">
+      <h1 class="mdc-typography--display1">Temporary Drawer</h1>
+      <p class="mdc-typography--body1">Click the menu icon above to open.</p>
+      <button v-on:click="fetchContent">I AM A FAT BUTTON</button>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+      fetchContent() {
+        console.log(this)
+        this.$http.get('http://localhost:49977/api/Test', {
+
+        }).then(response => {
+          console.log(response)
+        }, response => {
+          console.log('Fetch Failed')
+        })
+      }
+    }
+}
+import ResourceSchema from './ResourceSchema'
+console.log(new ResourceSchema(2,2));
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
